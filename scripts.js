1a63335666fb9e94ae534f3d446d0aaa9c8ea38c
mentyrop <40073070+mentyrop@users.noreply.github.com>
2018-06-08 23:18:03 +0300

    For Site

---
+++ scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  $('#mbut').click(function(){
+    $('.container').css({'background' : 'url("kot-diememe.jpg") no-repeat', 'background-size' : 'contain'});
+    $('#mbut').css('display', 'none');
+    $('#save-1').removeClass('hide')
+
+  })
+
+  $('#save-1').click(function(){
+    $('.container').css('background-image', 'url("end.jpg")');
+    $('#save-1').addClass('hide');
+    $('#save-2').removeClass('hide');
+    $('#sleep').removeClass('hide');
+  })
+
+  $('#save-2').click(function(){
+
+    if (confirm('НАЖМИ ДА! ИНАЧЕ Я САМ ЗАКРОЮ ОКНО!')) {
+      window.close();
+    } else {
+      window.close();
+    }
+  })
+})
